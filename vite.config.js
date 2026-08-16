c1065e0a3da6f2cd81d48bb7425b435d9c0c6350
farjana-10 <farjanafarjo10@gmail.com>
2026-08-17 02:24:01 +0600

    @

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { fileURLToPath } from 'url'  // <--- নতুন লাইন
+import { fileURLToPath } from 'url'
 
-// __dirname এর পরিবর্তে import.meta.dirname ব্যবহার করা
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
